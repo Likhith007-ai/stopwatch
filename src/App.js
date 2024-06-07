1c4adf5e3e2d1c8e282482bf9fb076bb5fa65619
Likhith <likhithsai7979@gmail.com>
2024-06-07 16:45:58 +0530

    stopwatch code updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
-function App() {
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    if (running) {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else if (!running && time !== 0) {
+      clearInterval(intervalRef.current);
+    }
+    return () => clearInterval(intervalRef.current);
+  }, [running]);
+
+  const startHandler = () => {
+    setRunning(true);
+  };
+
+  const stopHandler = () => {
+    setRunning(false);
+  };
+
+  const resetHandler = () => {
+    setRunning(false);
+    setTime(0);
+  };
+
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${hours > 0 ? `${hours}:` : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            fontSize: "80px",
+            color: "darkgrey",
+            fontFamily: "revert-layer",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Stopwatch
+        </h1>
+        <h2
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            fontSize: "50px",
+            color: "darkgrey",
+            fontFamily: "revert-layer",
+          }}
+        >
+          Time
+        </h2>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            fontSize: "250px",
+            marginTop: "35px",
+            color: "wheat",
+          }}
+        >
+          {formatTime(time)}
+        </h1>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "-90px",
+        }}
+        className="button-styling"
+      >
+        <button onClick={startHandler}>Start</button>
+        <button onClick={stopHandler}>Stop</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+      <p style={{ color: "white", fontFamily: "cursive", textAlign: "center" }}>
+        Developed by Likhith❤️
+      </p>
     </div>
   );
-}
+};
 
-export default App;
+export default Stopwatch;
